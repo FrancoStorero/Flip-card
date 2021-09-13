83b54bf7970d4a1bb29bd834e041492040f8c7f8
FrancoStorero <76909053+FrancoStorero@users.noreply.github.com>
2021-09-13 18:47:48 -0300

    Proyecto carta que gira

---
+++ main.js
@@ -0,0 +1,17 @@
+const girar = document.querySelector('.card__inner');
+
+girar.addEventListener('click', function(){
+    girar.classList.toggle('flip');
+    console.log('holaa')
+})
+
+/* ------Prueba con Boton-------- */
+
+/* const voltear = document.querySelector('#voltear');
+
+voltear.addEventListener('click', ()=>{
+    const card = document.querySelector('.card__inner');
+    card.classList.toggle('flip');    
+}) */
+
+
